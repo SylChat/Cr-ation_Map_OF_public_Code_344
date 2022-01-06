e8b4d9d822a23b2c60efa59f07e7d43856fa57ed
SylChat <97187606+SylChat@users.noreply.github.com>
2022-01-06 13:00:40 +0100

    Update CreationMapOF344.py

--- CreationMapOF344.py
+++ CreationMapOF344.py
@@ -3,24 +3,34 @@
 ########################################
 #Assignation du chemin des fichiers
 import urllib
+
+#définition du lien du fichier csv
 urlFichCsvOF = "https://www.monactiviteformation.emploi.gouv.fr/mon-activite-formation/public/listePubliqueOF?format=csv"
-urllib.request.urlretrieve(urlFichCsvOF,'/content/drive/MyDrive/OF.csv')
-sCSV = "/content/drive/MyDrive/OF.csv"
-dCSV = "/content/drive/MyDrive/OF_Filtres.csv"
+
+#téléchargement du fichier et enregistrement dans le répertoire
+urllib.request.urlretrieve(urlFichCsvOF,'OF.csv')
+
+#Assignation des variables pour les noms des fichiers
+sCSV = "OF.csv"
+dCSV = "OF_Filtres.csv"
 
 #df = dataframe issu du fichier source (sCSV)
 df = pd.read_csv(sCSV, sep=";", dtype="str")
 
 #dfTrier = dataframe qui recevra les données triées
+#dfTemp = dataframe transitoire
 CodeSST = "344"
 dfTemp = df.loc[((df[' informationsDeclarees.specialitesDeFormation.codeSpecialite1'] == CodeSST)|
                  (df[' informationsDeclarees.specialitesDeFormation.codeSpecialite2'] == CodeSST)| 
                  (df[' informationsDeclarees.specialitesDeFormation.codeSpecialite3'] == CodeSST))]
-dfFiltree=dfTemp[pd.notnull(dfTemp[' adressePhysiqueOrganismeFormation.ville'])]
+dfTemp=dfTemp[pd.notnull(dfTemp[' adressePhysiqueOrganismeFormation.ville'])]
 
-#Sélection des colonnes
-selected_column = dfFiltree.iloc[:,[2,3,5,6,7]]
+#Sélection des colonnes 2,3,5,6,7
+selected_column = dfTemp.iloc[:,[2,3,5,6,7]]
+
+#Copie de la sélection dans le dataframe dfTrier
 dfTrier = selected_column.copy()
+
 #boucle sur chaque pour geocoder les adresses
 for i, row in dfTrier.iterrows():
   adresse = str(row[' adressePhysiqueOrganismeFormation.voie']) + ", " + str(row[' adressePhysiqueOrganismeFormation.codePostal']) + ", " + str(row[' adressePhysiqueOrganismeFormation.ville'])
@@ -67,18 +77,19 @@ def LatLong(Ad: str):
 #importation des librairies
 import folium
 import pandas as pd
+
 #Définition du chemin du CSV
-CSV="/content/drive/MyDrive/OF_Filtres.csv"
+CSV="OF_Filtres.csv"
+
 #Création du dataframe
 df_source=pd.read_csv(CSV,sep=",")
 
 #Création de la maps vide centrée sur la france
 m = folium.Map(location=[45,5], zoom_start=6)
 
-#Ajout des points à la map
+#Ajout des points à la map en bouclant sur les lignes du csv
 for index, row in df_source.iterrows():
-  #print(row[' denomination'], row['GPSX'],row['GPSY'])
-  folium.Marker(location=[row['GPSX'],row['GPSY']],popup=row[' denomination']+ row[' adressePhysiqueOrganismeFormation.voie']).add_to(m)
+  folium.Marker(location=[row['GPSX'],row['GPSY']],popup=row[' denomination'] + " " + row[' adressePhysiqueOrganismeFormation.voie']).add_to(m)
 
 #show map
 m.save('/content/drive/MyDrive/Maps.html')
